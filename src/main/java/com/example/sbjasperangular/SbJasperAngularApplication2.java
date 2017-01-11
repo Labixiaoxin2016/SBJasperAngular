03ed93802c3db7cf6fc5d9d2500f456c270188fc
hidetarou2013@Hidelico-PC <hidetoshi_maekawa@e-it.co.jp>
2017-01-11 13:22:09 +0900

    refs #1 modified seedproject

--- src/main/java/com/example/sbjasperangular/SbJasperAngularApplication2.java
+++ src/main/java/com/example/sbjasperangular/SbJasperAngularApplication2.java
@@ -2,9 +2,11 @@ package com.example.sbjasperangular;
 
 import java.util.Locale;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +18,8 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
@@ -37,7 +41,7 @@ public class SbJasperAngularApplication2 extends WebMvcConfigurerAdapter {
 	@Bean
 	public LocaleResolver localeResolver() {
 		SessionLocaleResolver slr = new SessionLocaleResolver();
-		slr.setDefaultLocale(Locale.ENGLISH);
+		slr.setDefaultLocale(Locale.JAPANESE);//Locale.ENGLISH
 		return slr;
 	}
 
@@ -73,4 +77,21 @@ public class SbJasperAngularApplication2 extends WebMvcConfigurerAdapter {
 
 		}
 	}
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @Bean
+    public LocalValidatorFactoryBean validator()
+    {
+        LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
+        localValidatorFactoryBean.setValidationMessageSource(messageSource);
+        return localValidatorFactoryBean;
+    }
+
+    @Override
+    public Validator getValidator()
+    {
+        return validator();
+    }
 }
