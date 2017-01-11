0eab938f2fbc1d4c24332495c9662e721b31d555
hidetarou2013@Hidelico-PC <hidetoshi_maekawa@e-it.co.jp>
2017-01-11 11:30:12 +0900

    refs #1 hello world !

--- src/main/java/com/example/sbjasperangular/SbJasperAngularApplication.java
+++ src/main/java/com/example/sbjasperangular/SbJasperAngularApplication.java
@@ -1,12 +1,19 @@
 package com.example.sbjasperangular;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SbJasperAngularApplication {
+public class SbJasperAngularApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SbJasperAngularApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("hello world !");
 	}
 }
