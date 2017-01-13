b502c4bf05b8156c62664c70081042035d301e4a
name = hidetoshi_maekawa@maekawa-PC <hidetoshi_maekawa@e-it.co.jp>
2017-01-13 19:21:17 +0900

    refs #1 modified CityCountryRepositoryRestCassandra

--- src/main/java/com/example/sbjasperangular/repository/RepositoryConfiguration.java
+++ src/main/java/com/example/sbjasperangular/repository/RepositoryConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 
+import com.example.sbjasperangular.cassandra.model.CityCountry;
 import com.example.sbjasperangular.model.City;
 import com.example.sbjasperangular.model.Country;
 
@@ -13,7 +14,7 @@ import com.example.sbjasperangular.model.Country;
 public class RepositoryConfiguration extends RepositoryRestMvcConfiguration {
     @Override
     protected void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-	config.exposeIdsFor(City.class, Country.class);
+	config.exposeIdsFor(City.class, Country.class,CityCountry.class);
 //	config.setBaseUri("/rest");// メソッド setBaseUri(String) は型 RepositoryRestConfiguration で未定義です
 	config.setBasePath("/rest");
 
