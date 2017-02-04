661489ac5e1f035fd054e262997da2b125923549
hidetarou2013@Hidelico-PC <hidetoshi_maekawa@e-it.co.jp>
2017-02-04 14:42:30 +0900

    refs #1 #6 modified src/main/java/com/example/sbjasperangular/cassandra/repository/CassandraMLabelRepository.java

--- src/main/java/com/example/sbjasperangular/cassandra/repository/CassandraMLabelRepository.java
+++ src/main/java/com/example/sbjasperangular/cassandra/repository/CassandraMLabelRepository.java
@@ -12,24 +12,24 @@ import com.example.sbjasperangular.cassandra.model.CassandraMLabel;
 public interface CassandraMLabelRepository extends CassandraRepository<CassandraMLabel> {
 
 
-	@Query("SELECT * FROM M_LABEL where pkey1='tokyo' and pkey2='autobacks'")
+	@Query("SELECT * FROM M_LABEL where pkey1='autobacks' and pkey2='kokyaku'")
     List<CassandraMLabel> findAll();
 
 //	@Query("SELECT * FROM M_LABEL WHERE labelName=?0")
-	@Query("SELECT * FROM M_LABEL WHERE pkey1='tokyo' and pkey2='autobacks' and labelName=?0")
+	@Query("SELECT * FROM M_LABEL WHERE pkey1='autobacks' and pkey2='kokyaku' and labelName=?0")
 	public CassandraMLabel findByLabelName(String s);
 
-//	@Query("SELECT * FROM M_LABEL WHERE pkey1='tokyo' and pkey2='autobacks' and population=?0")
+//	@Query("SELECT * FROM M_LABEL WHERE pkey1='autobacks' and pkey2='kokyaku' and population=?0")
 //	public List<CassandraMLabel> findByPopulation(String s);
 //
 //
 ////	@Query("DELETE FROM M_LABEL WHERE name=?0")
-//	@Query("DELETE FROM M_LABEL WHERE pkey1='tokyo' and pkey2='autobacks' and name=?0")
+//	@Query("DELETE FROM M_LABEL WHERE pkey1='autobacks' and pkey2='kokyaku' and name=?0")
 //	public List<CassandraMLabel> deleteByName(String s);
 //
 //
 ////	@Query("SELECT * FROM M_LABEL WHERE name=?0 LIMIT ?1")
-//	@Query("SELECT * FROM M_LABEL WHERE pkey1='tokyo' and pkey2='autobacks' and country_id=?0")
+//	@Query("SELECT * FROM M_LABEL WHERE pkey1='autobacks' and pkey2='kokyaku' and country_id=?0")
 //    Iterable<CassandraMLabel> findByCountryId(String name);
 
 
